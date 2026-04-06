ef774fb63b3180adac5b216e519a7d6079d9d225
uma144 <umachouhan144@gmail.com>
2026-04-06 21:52:29 +0530

    update

--- js.html/Expreiment.js
+++ js.html/Expreiment.js
@@ -1,15 +1,14 @@
-const array = [0,1,2,6];
-//array.pop();
-//console.log(array);//[ 0, 1, 2 ]  //piche se value hatana
 
-const array2 = [0,1,2,6];
-//array2.push(0);
-//console.log(array2)//[ 0, 1, 2, 6 ]  //piche se value add
+const user = {
+  name: "Aman",
+  address: {
+    city: "Delhi"
+  }
+};
 
-const array3 = [0,1,2,6];
-//array3.unshift(2);
-//console.log(array3)  //aage se value add
+Object.freeze(user.address);
 
-const array4 = [0,1,2,6];
-array4.shift();
-console.log(array4);
+
+user.address.city = "Mumbai"; // ✅ change ho jayega!
+user.name ="rohit";
+console.log(user)
